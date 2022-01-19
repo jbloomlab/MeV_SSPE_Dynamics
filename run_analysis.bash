64286265a6200d22e1cccdfcaf67e06f534564a8
Will Hannon <wwh22@uw.edu>
2022-01-19 10:09:46 -0800

    Added the environment and cluster configuration files for FHCRC cluster.

---
+++ run_analysis.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --time 1-0
+
+# stop on errors
+set -e
+
+echo "Running snakemake..."
+
+# Remove tmp/ dir and slurm files
+snakemake clean --cores 1
+
+# Make fresh tmp/ dir 
+mkdir -p tmp
+
+# Run the main analysis on `slurm` cluster
+snakemake \
+    --use-conda \
+    --conda-frontend mamba \
+    --conda-prefix ./env \
+    -j 999 \
+    --cluster-config cluster.yml \
+    --cluster "sbatch -p {cluster.partition} -c {cluster.cpus} -t {cluster.time} -J {cluster.name} --mail-user={cluster.user} --mail-type={cluster.mailtype} -o ./tmp/slurm-%x.%j.out" \
+    --latency-wait 60
+
+echo "Run of snakemake complete."
+
+echo "Generating report..."
+snakemake --report results/report.html
+echo "Report created."
