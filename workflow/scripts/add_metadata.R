b5a40ebeef872ad0bfe9ea4caea0a92be0368f93
Will Hannon <wwh22@uw.edu>
2022-01-27 10:56:08 -0800

    Workflow is working up to haplotype calling.

---
+++ workflow/scripts/add_metadata.R
@@ -0,0 +1,157 @@
+## ---------------------------
+##
+## Script name: `rule add_metadata`
+##
+## Purpose of script: read in file with given accession and add in the requisite metadata
+##
+## Author: Will Hannon
+##
+## Date Created: 2020-06-26
+##
+## Copyright (c) Will Hannon, 2020
+## Email: wwh22@uw.edu
+##
+## ---------------------------
+
+require(tidyverse)
+
+## ---------------------------
+
+## ==== Get file paths from snakemake object ==== ##
+
+vcf.filepath = snakemake@input[[1]] 
+metadata.filepath = snakemake@params[[1]] 
+
+## ==== Boolean to see if file is empty and there are no columns ==== ##
+info = file.info(vcf.filepath)
+empty = (info$size == 0)
+
+## ==== Import Metadata, same table used to import samples from `Run Selector w/ minimal changes` ==== ##
+#
+# Add in any constrains on metadata here.
+#
+# Download and format metedata. But first, check if the vcf is empty..
+if (!empty){
+  
+  metadata.df = read_csv(metadata.filepath) %>% rename(Accession = "Run")
+  
+  ## ==== Import and process the vcf -> txt files ==== ##
+  
+  # cols to split `ANN` vcf field into
+  cols_into = c("ALT_Allele", 
+                "Effect",
+                "Putative_Impact",
+                "Gene_Name",
+                "Gene_ID",
+                "Feature_Type",
+                "Feature_ID",
+                "Transcript_Biotype",
+                "Rank",
+                "HGVS.c",
+                "HGVS.p",
+                "cDNA_position",
+                "CDS_position", 
+                "Protein_position",
+                "Distance_to_feature",
+                "Errors")
+  
+  # extract metadata from filepath 
+  sample.info = strsplit(basename(vcf.filepath), "[.]")[[1]]
+  sample.name = sample.info[1]
+  sample.caller = sample.info[2]
+
+  # different extraction depending on columns in the original VCF file
+  if (sample.caller == "lofreq"){
+    # read in vcf table
+    sample.df = read_tsv(vcf.filepath) %>% 
+      # split into cols with regex
+      separate(col = ANN, sep = "\\|", remove = F, into = cols_into) %>% 
+      select(POS, REF,  ALT,
+             AF,  DP,
+             Effect, Gene_Name, AA_Change="HGVS.p") %>% 
+      # clean amino acid change column
+      mutate(AA_Change = gsub("^p\\.", "", AA_Change)) %>% 
+      # simplify the effect column
+      mutate(Effect = case_when(Effect  == "missense_variant" ~ "Missense",
+                                Effect  == "synonymous_variant" ~ "Synonymous",
+                                Effect  ==  "upstream_gene_variant"  ~ "Synonymous",
+                                Effect  ==  "downstream_gene_variant"  ~ "Synonymous",
+                                Effect  ==  "stop_retained_variant"  ~ "Synonymous",
+                                Effect  ==  "stop_lost" ~ "Nonsense",
+                                Effect  ==  "start_lost"  ~ "Nonsense",
+                                Effect  ==  "stop_gained" ~ "Nonsense")) %>% 
+      # add sample ID
+      mutate(Accession = sample.name) %>%
+      mutate(Caller = sample.caller) 
+    
+  } else if (sample.caller == "varscan"){
+    
+    # read in vcf table
+    sample.df = read_tsv(vcf.filepath) %>%
+      # split into cols with regex
+      separate(col = ANN, sep = "\\|", remove = F, into = cols_into) %>% 
+      select(POS, REF,  ALT,
+             AF="Sample1.FREQ",  DP="Sample1.DP",
+             Effect, Gene_Name, AA_Change="HGVS.p") %>% 
+      # clean amino acid change column
+      mutate(AA_Change = gsub("^p\\.", "", AA_Change)) %>% 
+      # clean allele freq column
+      mutate(AF = gsub("%", "", AF)) %>% 
+      # simplify the effect column
+      mutate(Effect = case_when(Effect  == "missense_variant" ~ "Missense",
+                                Effect  == "synonymous_variant" ~ "Synonymous",
+                                Effect  ==  "upstream_gene_variant"  ~ "Synonymous",
+                                Effect  ==  "downstream_gene_variant"  ~ "Synonymous",
+                                Effect  ==  "stop_retained_variant"  ~ "Synonymous",
+                                Effect  ==  "stop_lost" ~ "Nonsense",
+                                Effect  ==  "start_lost"  ~ "Nonsense",
+                                Effect  ==  "stop_gained" ~ "Nonsense")) %>% 
+      # add sample ID
+      mutate(Accession = sample.name) %>%
+      mutate(Caller = sample.caller) 
+    
+    sample.df$AF = as.numeric(sample.df$AF) / 100
+    
+  } else if (sample.caller == "ivar"){
+    
+    # read in vcf table
+    sample.df = read_tsv(vcf.filepath) %>%
+      mutate(Effect = if_else(REF_AA == ALT_AA, "Synonymous", "Missense"),
+             AA_Change = paste0(REF_AA, "-", ALT_AA),
+             Gene_Name = NA) %>% 
+      select(POS, REF,  ALT,
+             AF="ALT_FREQ",  DP="TOTAL_DP",
+             Effect, Gene_Name, AA_Change) %>% 
+      # add sample ID
+      mutate(Accession = sample.name) %>%
+      mutate(Caller = sample.caller) %>% 
+      distinct()
+    
+  }
+  
+  ## ==== Join the variant data with the metadata by 'Run' id ==== ##
+  
+  # Join with metadata
+  sample.df = left_join(sample.df, metadata.df, by = "Accession")
+  
+  # Write out to a file
+  write.csv(sample.df, file = snakemake@output[[1]], row.names=FALSE, sep="\t")
+  
+} else{
+  # Write an empty output if file is empty
+  cat(NULL,file=snakemake@output[[1]])
+}
+
+
+## ==== END ==== ##
+
+
+
+
+
+
+
+
+
+
+
---
+++ workflow/scripts/make-sspe-reference.py
@@ -0,0 +1,317 @@
+"""
+The goal of this script is to make an SSPE consensus sequence for a variant calling reference. 
+
+The SSPE consensus will only include the mutations that are fixed in most of the SSPE samples. 
+"""
+
+__author__ = "Will Hannon"
+__copyright__ = "Copyright 2021 Will Hannon"
+__email__ = "wwh22@uw.edu"
+__license__ = "MIT"
+
+from Bio import SeqIO #reading fasta format
+import pysam #count variant alleles from BAM
+import pandas as pd #data frames
+import concurrent.futures #multithreadding manager
+import functools #handle function arguments in map
+import numpy as np
+import os 
+from collections import Counter
+
+
+def get_ref_sequence(path):
+    """
+    Function imports the reference sequence as a list of bases. 
+
+    Parameters
+    ----------
+    path : str
+        Path to reference sequence file - should be a fasta file. 
+
+    Returns
+    -------
+    list
+        List of bases in the reference sequence.
+        
+    """
+    return [base.upper() for base in list(SeqIO.parse(path, "fasta"))[0].seq]
+
+
+def split(refseq, threads):
+    """
+    Function split reference into approxamatley equal sized chunks
+    based on the number of threads alloted.
+
+    Parameters
+    ----------
+    refseq : list
+       Reference sequence.
+       
+    threads: int
+        Integer to the number of threads alloted. 
+
+    Returns
+    -------
+    list
+        List of tuples containing the start and end position for coverage. 
+        
+    """
+    
+    k, m = divmod(len(refseq), threads)
+    
+    return [( i * k + min(i, m), (i+1) * k + min(i+1, m)) for i in range(threads)]
+
+
+def check_read(read):
+    """
+    Helper function to decide what reads should
+    be keep when parsing alignment file with `pysam`. 
+
+    Parameters
+    ----------
+    read : AlignedSegment
+        read from alignment file parsed with `pysam`.
+
+    Returns
+    -------
+    bool
+        True/False if read should be included
+        
+    """
+    # Exclude Quality Failures
+    if read.is_qcfail:
+        return False
+    # Exclude Secondary Mappings
+    if read.is_secondary:
+        return False
+    # Exclude Unmapped Reads
+    if read.is_unmapped:
+        return False
+    else:
+        return True
+    
+
+def count_coverage(bampath,
+                   contig, 
+                   refpath, 
+                   region,
+                   callback_function = check_read, 
+                   minimum_QUAL = 25): 
+    """
+    Helper function to decide what reads should
+    be keep when parsing alignment file with `pysam`. 
+
+    Parameters
+    ----------
+    read : AlignedSegment
+        read from alignment file parsed with `pysam`.
+
+    Returns
+    -------
+    bool
+        True/False if read should be included
+        
+    """
+
+    # Open alignment with pysam
+    with pysam.AlignmentFile(bampath, "rb") as bamfile:
+        # Get a dataframe of the counts
+        count_df = pd.DataFrame.from_dict({base:counts for base, counts in zip("ACGT",
+                                                                               bamfile.count_coverage(contig = contig,
+                                                                                                      start = region[0], 
+                                                                                                      stop = region[1],
+                                                                                                      read_callback = callback_function,
+                                                                                                      quality_threshold = minimum_QUAL))})
+        # Add the depth at each position
+        count_df['DP'] = count_df.sum(axis = 1)
+        # Add the position 
+        count_df['POS'] = [pos + 1 for pos in range(region[0], region[1])]
+        # Add the reference allele
+        count_df['REF'] = get_ref_sequence(refpath)[region[0]:region[1]]
+        # Convert counts to frequency 
+        count_df.iloc[:,0:4] = count_df.iloc[:,0:4].div(count_df.DP, axis = 0)
+        # Handle any NaNs created by dividing by 0 coverage
+        count_df = count_df.fillna(0)
+    
+    return count_df
+
+    
+def call_variants(bampath,
+                  refpath,
+                  contig,
+                  threads,
+                  minimum_QUAL = 25,
+                  minimum_COV = 100):
+    """
+    Read in BAM file and convert to a dataframe containing the frequency of 
+    of any bases present at a given position in the reference genome using the
+    `pysam` command `count_coverage`. 
+
+    Parameters
+    ----------
+    filepath : str
+        path to the bam file to be parsed
+        
+    callback_function : function
+        function that decides which reads to keep/exclude
+        
+    ref : str
+        name of the contig to count coverage over
+        
+    ref_path : str
+        path to the reference genome as fasta
+         
+    minimum_qual : int
+        minimum QUAL score to count base observation at a position.
+
+    Returns
+    -------
+    Pandas.DataFrame
+       Data Frame containing the bases represented at each positon in the genome
+        
+    """
+    
+    # Regions for parallelization
+    regions = split(get_ref_sequence(refpath), threads)
+
+    # Multithreadding to make the count dataframe 
+    with concurrent.futures.ProcessPoolExecutor() as executor: 
+
+        results = executor.map(functools.partial(count_coverage,
+                                                 bampath,
+                                                 contig,
+                                                 refpath,  
+                                                 minimum_QUAL = minimum_QUAL), 
+                               regions)
+
+        count_df = pd.concat([result for result in results]).reset_index()
+    
+    
+
+    # Melt the data frame to a longer ('tidy') form
+    count_df = pd.melt(count_df, 
+                       id_vars=['POS', 'DP', 'REF'],
+                       value_vars=[base for base in 'ATGC'],
+                       value_name='AF',
+                       var_name='ALT')
+    
+    # Remove anything with 0 coverage.
+    count_df = count_df[count_df['AF'] > 0]
+    # TRUE/FALSE if it's a SNP
+    count_df['SNP'] = np.where(count_df['ALT'] != count_df['REF'], True, False)
+    # TRUE/FALSE if it's a consensus base.
+    count_df['CONS'] = count_df['AF'].map(lambda x: x >= 0.5)
+    # Add the number of times a given allele is observed.
+    count_df['OBSV'] = count_df.DP * count_df.AF
+    # Filter to only get SNPs
+    count_df = count_df.loc[count_df['SNP']]
+
+    # Merge the pileup dict and the snp dict. 
+    return count_df
+
+
+def update_reference(bampaths, 
+                     refpath,
+                     outpath,
+                     contig, 
+                     threads,
+                     fixed_threshold = .95,
+                     minimum_QUAL = 25,
+                     minimum_COV = 100):
+    """
+    Update the current MevChiTok reference sequence by incorporating fixed mutations present
+    in the alignments to the original reference.
+
+    Parameters
+    ----------
+    bampaths : list
+        a list of paths to the bam file to be parsed
+        
+    refpath : str
+        path to the reference genome as fasta     
+       
+    outpath : path
+        path to the updated reference fasta
+        
+    contig : str
+        name of the contig to count coverage over
+        
+    threads : int
+        number of threads to call variants in parallel
+        
+    fixed_threshold : int
+        minimum AF to be considered a fixed mutation.
+         
+    minimum_QUAL : int
+        minimum QUAL score to count base observation at a position.
+        
+    minimum_COV : int
+        minimum DP score to count base observation at a position.
+
+    """
+    
+    # Go through and call variants in each bam file.
+    variant_df_list = []
+    for bampath in bampaths: 
+        # Call the variants from the alignment
+        variant_df = call_variants(bampath,
+                              refpath,
+                              contig,
+                              threads,
+                              minimum_QUAL = 25,
+                              minimum_COV = 100)
+        # Recover the fixed variants 
+        variant_df = variant_df[variant_df['AF'] >= fixed_threshold]
+        # Append the sample name
+        variant_df["Tissue"] = " ".join(os.path.basename(bampath).split(".")[0].split("_"))
+        # Append the variant df to the list
+        variant_df_list.append(variant_df)
+    
+    # Make a single dataframe with every fixed variant
+    all_fixed_variants_df = pd.concat(variant_df_list)
+    
+    # Get the fixed SNPs and their total count in the samples 
+    fixed_snps = Counter((pos, alt) for pos, alt in zip(all_fixed_variants_df.POS, all_fixed_variants_df.ALT))
+    
+    # Get the SNPs deemed to be in enough samples to be included in the updated reference
+    consensus_snps = [snp for snp, count in fixed_snps.items() if count >= 12]
+    
+    # Update the reference sequence 
+    ref_seq = get_ref_sequence(refpath)
+
+    for pos, base in consensus_snps: 
+        ref_seq[pos-1] = base
+
+    # Write the updated reference out to a fasta file. 
+    with open(outpath, "w") as outfile:
+        outfile.write(">" + "MeVChiTok" + "\n" + "".join(ref_seq) + "\n")
+        
+    print("Done!")
+
+
+def main():
+
+    # Get the path list for the input BAM files from snakemake rule.
+    bampaths = snakemake.input.bam
+    # Get the path to the reference genome.
+    refpath = str(snakemake.input.genome)
+    # Get the path to the output multi-fasta file. 
+    outpath = str(snakemake.output)
+    # Get the contig
+    contig = str(snakemake.params.contig)
+    # Get the number of threads to use
+    threads = int(snakemake.threads)
+
+    update_reference(bampaths, 
+                    refpath,
+                    outpath,
+                    contig, 
+                    threads,
+                    fixed_threshold = .95,
+                    minimum_QUAL = 25,
+                    minimum_COV = 100)
+
+if __name__ == '__main__':
+    main()
+
+
